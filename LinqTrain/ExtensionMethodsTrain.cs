f807d9ec8d198bd71503f054c038066c7ce982ef
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2020-10-12 00:05:34 -0400

    Reimplemetation of linq enumerable methods

---
+++ LinqTrain/ExtensionMethodsTrain.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections.Generic;
+
+namespace LinqTrain
+{
+    ///<summery>
+    /// Extension Methods Reimplementation.
+    public static class ExtensionMethodsTrain 
+    {
+        /// <summary>
+        /// Filters a sequence of values based on a predicate        
+        /// </summary>
+        public static IEnumerable<TSource> myWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, Boolean> predicate)
+        {
+            foreach (var item in source)
+            {
+                if(predicate(item))
+                {
+                    yield return item;
+                }                
+            }
+        }
+
+        ///<summary>
+        /// Filters a sequence of values based on a predicate. Each element's index is used in the logic of the predicate function.
+        ///</summary>
+        public static IEnumerable<TSource> myWhere<TSource> (this IEnumerable<TSource> source, Func<TSource,int,bool> predicate)
+        {
+                int i = 0;
+
+            foreach (var item in source)
+            {
+                if (predicate(item, i))
+                {
+                    yield return item;                    
+                }
+                i++;                
+            }
+        }
+        ///<summary>
+        ///Projects each element of a sequence into a new form
+        ///</summary>\
+        public static IEnumerable<TResult> mySelect<TSource,TResult> (this IEnumerable<TSource> source, Func<TSource,TResult> selector)
+        {
+            foreach (var item in source)
+            {
+                yield return selector(item);                
+            }
+
+        }
+
+        ///<summary>
+        /// Projects each element in the sequence into a new form by incorporating the element's index.
+        ///</summary>
+        public static IEnumerable<TResult> mySelect<TSource,TResult>(this IEnumerable<TSource> source, Func<TSource,Int32,TResult> selector)
+        {
+            var i = 0;
+            foreach (var item in source)
+            {
+                yield return selector(item, i);                
+            }
+            i++;
+
+        }
+
+        ///<summary>
+        /// Projects each element of a sequence to an IEnumerable<T> and flattens the resulting sequences into one sequence.
+        ///</summary>
+        public static IEnumerable<TResult> mySelectMany<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (var item in source)
+            {
+                foreach (var result in selector(item))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        ///<summary>
+        /// Projects each element of a sequence to an IEnumerable<T>, flattens the resulting sequences into one sequence, 
+        ///and invokes a result selector function on each element therein.
+        ///</summary>
+        public static IEnumerable<TResult> mySelectMany<TSource,TCollection,TResult> (
+            this IEnumerable<TSource> source, 
+            Func<TSource,IEnumerable<TCollection>> collectionSelector, 
+            Func<TSource,TCollection,TResult> resultSelector)
+        {
+             foreach (var item in source)
+            {
+                foreach (var collectionItem in collectionSelector(item))
+                {
+                    yield return resultSelector(item, collectionItem);
+                }
+            }
+        }
+
+
+        ///<summary>
+        /// Projects each element of a sequence to an IEnumerable<T>, flattens the resulting sequences into one sequence, 
+        /// and invokes a result selector function on each element therein. 
+        /// The index of each source element is used in the intermediate projected form of that element.
+        ///</summary>
+        public static IEnumerable<TResult> mySelectMany<TSource, TCollection, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            int index = 0;
+            foreach (var item in source)
+            {
+                foreach (var collectionItem in collectionSelector(item, index++))
+                {
+                    yield return resultSelector(item, collectionItem);
+                }
+            }
+        }
+
+        ///<summary>
+        /// Projects each element of a sequence to an IEnumerable<T>, and flattens the resulting sequences into one sequence. 
+        /// The index of each source element is used in the projected form of that element.
+        ///</summary>
+
+        public static IEnumerable<TResult> mySelectMany<TSource,TResult> (
+            this IEnumerable<TSource> source, 
+            Func<TSource,int,IEnumerable<TResult>> selector)
+        {
+            var i = 0;
+            foreach (var item in source)
+            { 
+                foreach (var collectionItem in selector(item, i++))
+                {           
+                    yield return collectionItem;  
+                }              
+            }            
+
+        }
+
+        ///<summary>
+        /// Creates a List<T> from an IEnumerable<T>.
+        /// A List<T> that contains elements from the input sequence.
+        ///</summary>
+        public static List<TSource> myToList<TSource> (
+        this IEnumerable<TSource> source)
+        => new List<TSource>(source);          
+        
+        
+        ///<summary>
+        ///
+        ///</summary>
+
+    //     public static IEnumerable<TSource> Reverse<TSource>(this IEnumerable<TSource> source) 
+    //     {
+    //         foreach (var item in source)
+    //         {
+    //             yield return item;
+    //         }
+    //         return ReverseIterator<TSource>(source);
+    //     }
+
+    // public static IEnumerable<TSource> ReverseIterator<TSource>( this IEnumerable<TSource> source) 
+    // { 
+    //     var buffer = new Buffer<TSource>(source);
+    //     for (int i = buffer.count - 1; i >= 0; i--) 
+    //     yield return buffer.items[i]; 
+    // } 
+
+
+        
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqTrain;
+
+namespace CSharpTrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyWhereFilter();
+            MyWhereFilterWithIndex();
+            
+            MySelectProjection();
+            MySelectProjectionWithIndex();
+
+            MySelectManyProjection();
+        }
+
+        private static void MySelectManyProjection()
+        {
+            List<List<int>> numbers = new List<List<int>>
+            {
+                new List<int> { 23, 45, 66 },  //0 Index
+                new List<int> { 12, 88, 32 }  //1 Index
+            };
+            List<int> result = numbers.mySelectMany((x, i) => x.mySelect(z => z + i)).myToList();
+        }
+
+        private static void MySelectProjection()
+        {
+            IEnumerable<int> squares =
+                            System.Linq.Enumerable.Range(1, 10).mySelect(x => x * x);
+
+            foreach (var num in squares)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        private static void MySelectProjectionWithIndex()
+        {
+            string[] fruits = { "apple", "banana", "mango", "orange", "passionfruit", "grape" };
+
+            var query =
+                fruits.mySelect((fruit, index) =>
+                                  new { index, str = fruit.Substring(0, index) });
+
+            foreach (var obj in query)
+            {
+                Console.WriteLine("{0}", obj);
+            }
+        }
+
+        private static void MyWhereFilterWithIndex()
+        {
+            int[] numbers = { 0, 30, 20, 15, 90, 85, 40, 75 };
+
+            foreach (int number in numbers.myWhere((number, index) => number <= index * 10))
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        private static void MyWhereFilter()
+        {
+            List<int> num = new List<int>() { 2, 3, 5, 8, 10, 12, 15, 22, 30 };
+            Console.WriteLine($"Filtering event numbers from the list of numbers: \n");
+            foreach (var item in num.myWhere(n => n % 2 == 0))
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+
+}
