1fe3b73fb12472847f931d2c7eb461b2779b4157
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2020-11-22 22:31:25 -0500

    Refactor Names of class, method, Remove with R for a hashset

--- LinqTrain/SetOperations/InersectOperationsTrain.cs
+++ LinqTrain/SetOperations/InersectOperationsTrain.cs
@@ -6,12 +6,12 @@ namespace LinqTrain
     ///<summary>
     /// Returns the set intersection, which means elements that appear in each of two collections.
     ///</summary>
-    public static class ExceptOperationsTrain 
+    public static class IntersectOperationsTrain 
     {
         ///<summary>
        /// Produces the set intersection of two sequences by using the default equality comparer to compare values.
        ///</summary>
-       public static IEnumerable<TSource> Intersect<TSource> (
+       public static IEnumerable<TSource> myIntersect<TSource> (
            this IEnumerable<TSource> first, 
        IEnumerable<TSource> second)
        {
@@ -19,7 +19,7 @@ namespace LinqTrain
            HashSet<TSource> set = new HashSet<TSource>(second, EqualityComparer<TSource>.Default);           
            foreach (var item in first)
            {
-               if (set.remove(item))
+               if (set.Remove(item))
                 {
                     yield return item;
                 }
@@ -30,7 +30,7 @@ namespace LinqTrain
         ///<summary>
         /// Produces the set intersection of two sequences by using the specified IEqualityComparer<T> to compare values.
         ///</summary>
-        public static IEnumerable<TSource> Intersect<TSource> (
+        public static IEnumerable<TSource> myIntersect<TSource> (
             this IEnumerable<TSource> first, 
             IEnumerable<TSource> second, 
         IEqualityComparer<TSource>? comparer)
@@ -38,7 +38,7 @@ namespace LinqTrain
             HashSet<TSource> setSource = new HashSet<TSource>(second, comparer);
             foreach (TSource item in first)
             {
-                if (setSource.remove(item))
+                if (setSource.Remove(item))
                 {
                     yield return item;
                 }
