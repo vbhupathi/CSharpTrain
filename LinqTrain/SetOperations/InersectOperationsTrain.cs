8d3d223bb4c9b8d03aad29f7dcc9cabd34deb015
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2020-11-22 22:22:44 -0500

    Implement Intersect overload which Produces the set intersection of two sequences.

---
+++ LinqTrain/SetOperations/InersectOperationsTrain.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace LinqTrain
+{
+    ///<summary>
+    /// Returns the set intersection, which means elements that appear in each of two collections.
+    ///</summary>
+    public static class ExceptOperationsTrain 
+    {
+        ///<summary>
+       /// Produces the set intersection of two sequences by using the default equality comparer to compare values.
+       ///</summary>
+       public static IEnumerable<TSource> Intersect<TSource> (
+           this IEnumerable<TSource> first, 
+       IEnumerable<TSource> second)
+       {
+          // return first.myExcept(second);
+           HashSet<TSource> set = new HashSet<TSource>(second, EqualityComparer<TSource>.Default);           
+           foreach (var item in first)
+           {
+               if (set.remove(item))
+                {
+                    yield return item;
+                }
+           }  
+
+       }
+               
+        ///<summary>
+        /// Produces the set intersection of two sequences by using the specified IEqualityComparer<T> to compare values.
+        ///</summary>
+        public static IEnumerable<TSource> Intersect<TSource> (
+            this IEnumerable<TSource> first, 
+            IEnumerable<TSource> second, 
+        IEqualityComparer<TSource>? comparer)
+        {
+            HashSet<TSource> setSource = new HashSet<TSource>(second, comparer);
+            foreach (TSource item in first)
+            {
+                if (setSource.remove(item))
+                {
+                    yield return item;
+                }
+            }
+        }        
+    }
+
+    
+}
