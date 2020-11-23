5d87565a55b762f622c7cfcaeb16710761d1f748
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2020-11-22 21:31:18 -0500

    myDistinctOfIntegerSequence returns distinct elements from  sequence of integers.

--- Program.cs
+++ Program.cs
@@ -25,7 +25,10 @@ namespace CSharpTrain
     
         static void Main(string[] args)
         {
+            //Set Operations
+            myDistinctOfIntegerSequence();
 
+            //Concatenation Operation
             myConcatExample();
             //Max Operations
             MyMaxOfASequenceOfInt32();
@@ -60,6 +63,30 @@ namespace CSharpTrain
             MySelectManyProjectionWithTCollectionSelectorAndIndex();
             MySelectManyProjectionWithSelector();
             MySelectManyProjectionWithSelectorIndex();
+        }
+
+        private static void myDistinctOfIntegerSequence()
+        {
+            List<int> ages = new List<int> { 21, 46, 46, 55, 17, 21, 55, 55 };
+
+            IEnumerable<int> distinctAges = ages.myDistinct();
+
+            Console.WriteLine("Distinct ages:");
+
+            foreach (int age in distinctAges)
+            {
+                Console.WriteLine(age);
+            }
+
+            /*
+             This code produces the following output:
+
+             Distinct ages:
+             21
+             46
+             55
+             17
+            */
         }
        
         
