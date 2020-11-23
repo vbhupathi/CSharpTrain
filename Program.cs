0e58f76e7367cfb21e7b954bdaf8788f31eb776e
vbhupathi <50917935+vbhupathi@users.noreply.github.com>
2020-11-22 21:38:19 -0500

    myDistinctOfSpecifiedIEqualityComparerValues which returns the distinct elements from a sequence by using the default equality comparer to compare values.

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ namespace CSharpTrain
         {
             //Set Operations
             myDistinctOfIntegerSequence();
+            myDistinctOfSpecifiedIEqualityComparerValues();
 
             //Concatenation Operation
             myConcatExample();
@@ -63,6 +64,28 @@ namespace CSharpTrain
             MySelectManyProjectionWithTCollectionSelectorAndIndex();
             MySelectManyProjectionWithSelector();
             MySelectManyProjectionWithSelectorIndex();
+        }
+        private static void myDistinctOfSpecifiedIEqualityComparerValues()
+        {
+            Product[] products = { new Product { Name = "apple", Code = 9 },
+                       new Product { Name = "orange", Code = 4 },
+                       new Product { Name = "apple", Code = 9 },
+                       new Product { Name = "lemon", Code = 12 } };
+
+            //Exclude duplicates.
+
+            IEnumerable<Product> noduplicates =
+                products.myDistinct();
+
+            foreach (var product in noduplicates)
+                Console.WriteLine(product.Name + " " + product.Code);
+
+            /*
+                This code produces the following output:
+                apple 9
+                orange 4
+                lemon 12
+            */
         }
 
         private static void myDistinctOfIntegerSequence()
